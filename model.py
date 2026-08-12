22159d6fdc20bc34c49fc3c238797e7c2e987ef2
Avik Das <avikd957@gmail.com>
2026-08-12 15:00:45 +0530

    Step 1: init_q_table

--- model.py
+++ model.py
@@ -9,7 +9,6 @@ import numpy as np
 # Step 1 - init_q_table
 import numpy as np
 
-
 def init_q_table(num_states, num_actions):
     """Return a zero-initialized Q-table of shape (num_states, num_actions)."""
     # TODO: build a 2D float64 numpy array of zeros sized by states and actions.
